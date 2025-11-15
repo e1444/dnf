2e42bded08bd2439e9159e9484c0a3e5da81a5aa
e1444 <er.liang@mail.utoronto.ca>
2025-11-15 09:47:36 -0800

    Add training scripts

---
+++ scripts/train_fir.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+#SBATCH --account=def-six
+#SBATCH --job-name=DNF_train
+#SBATCH --gpus-per-node=h100:1
+#SBATCH --cpus-per-task=4
+#SBATCH --mem=32000M
+#SBATCH --time=2:00:00
+#SBATCH --mail-user=er.liang@mail.utoronto.ca
+#SBATCH --mail-type=ALL
+
+TIMESTAMP=$(date +"%Y-%m-%d/%H-%M-%S")
+LOG_DIR="${SCRATCH}/dnf/logs/${TIMESTAMP}"
+export WANDB_DIR="${SCRATCH}/dnf/wandb"
+
+mkdir -p ${LOG_DIR}
+
+exec > ${LOG_DIR}/slurm-${SLURM_JOB_ID}.out 2> ${LOG_DIR}/slurm-${SLURM_JOB_ID}.err
+
+module load StdEnv/2023
+module load python/3.11.5
+module load cuda/12.6
+
+# virtualenv --no-download --system-site-packages ENV --python=python3.11
+source ENV/bin/activate
+
+# pip install -r requirements.txt
+
+# Run the training script, overriding the Hydra run directory
+python -m src.train \
+    hydra.run.dir=${LOG_DIR} \
+    data.dataset.num_workers=${SLURM_CPUS_PER_TASK} \
+    model=dglow_resnet \
+    training.lr_means=0 \
+    training.lr_vars=0.001 \
+    # training.resume_from_checkpoint=logs/2025-11-14/13-23-09/checkpoint_epoch_50.pth
+
+# python -m src.train \
+#     --multirun \
+#     hydra.sweep.dir=${LOG_DIR} \
+#     data.dataset.num_workers=${SLURM_CPUS_PER_TASK} \
+#     +hpo=default
