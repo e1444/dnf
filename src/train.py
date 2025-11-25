d9c2c941ecd5560aefd1e83441130d0dba2dabe1
Eric Liang <eliangca@outlook.com>
2025-11-24 23:40:17 -0500

    Added default log alpha

--- src/train.py
+++ src/train.py
@@ -72,7 +72,7 @@ def train(cfg: DictConfig):
     
     # --- Augmented Lagrangian Setup ---
     # Initialize dual variable (log_alpha)
-    log_alpha = torch.tensor(0.0, requires_grad=True, device=device)
+    log_alpha = torch.tensor(cfg.training.log_alpha, requires_grad=True, device=device)
     # Separate optimizer for alpha (dual)
     alpha_optimizer = optim.Adam([log_alpha], lr=cfg.training.lr_log_alpha)
     
