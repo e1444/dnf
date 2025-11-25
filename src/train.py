e273b0b443b0e236c5e812b61956891754e53287
Eric Liang <eliangca@outlook.com>
2025-11-25 00:58:08 -0500

    Fixed bug with lr not updating

--- src/train.py
+++ src/train.py
@@ -105,13 +105,14 @@ def train(cfg: DictConfig):
             optimizer.param_groups[2]['lr'] = cfg.training.lr_v
             optimizer.param_groups[3]['lr'] = cfg.training.lr_U
             optimizer.param_groups[4]['lr'] = cfg.training.lr_df
-        start_epoch = checkpoint['epoch'] + 1
             
         # Load dual variables if available
         if 'log_alpha' in checkpoint:
             log_alpha.data.copy_(checkpoint['log_alpha'])
         if 'alpha_optimizer_state_dict' in checkpoint:
             alpha_optimizer.load_state_dict(checkpoint['alpha_optimizer_state_dict'])
+            alpha_optimizer.param_groups[0]['lr'] = cfg.training.lr_log_alpha
+        start_epoch = checkpoint['epoch'] + 1
         
     if cfg.training.reset_freeze:
         model.set_freeze_steps(freeze=False, start=0, end=-1)
