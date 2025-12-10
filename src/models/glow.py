978a619757d6f9742d38c0bf678e62d619385697
Eric Liang <eliangca@outlook.com>
2025-12-10 18:18:00 -0500

    Removed legacy code

--- src/models/glow.py
+++ src/models/glow.py
@@ -64,77 +64,10 @@ class Glow(nn.Module):
         log_dets = torch.stack(log_dets, dim=0)
         return outs, log_dets
     
-    def inverse(self, z):
-        # LEGACY: used to reconstruct from a list of tensors [z0, z1, ..., zN]
-        total_log_det = torch.zeros(z[0].shape[0], device=z[0].device)
-        x = z.pop()
-        
-        for level in reversed(self.split_levels):
-            if isinstance(level, nn.ModuleList): # Flow steps
-                for flow_step in reversed(level):
-                    x, log_det = flow_step.inverse(x)
-                    total_log_det += log_det
-                    
-                x, log_det_squeeze = self.squeeze.inverse(x)
-                total_log_det += log_det_squeeze
-            elif isinstance(level, Split): # Split
-                x, log_det = level.inverse(x, z.pop())
-                total_log_det += log_det
-                
-        x, log_det = self.logit_transform.inverse(x)
-        total_log_det += log_det
-
-        return x, total_log_det
-    
     @property
     def total_steps(self):
         return sum(self.steps_per_level)
 
 
 if __name__ == "__main__":
-    # --- Test FlowStep Invertibility ---
-    # print("--- Testing FlowStep Invertibility ---")
-    # x = torch.randn(8, 3, 32, 32).clamp(min=0.0, max=1.0)
-    # step = AffineCouplingLayer(
-    #     in_channels=3,
-    #     hidden_channels=64,
-    #     scale_clamp=1.0,
-    # )
-    # z, log_det = step(x)
-    # x_recon, log_det_inv = step.inverse(z)
-    
-    # recon_error_step = torch.abs(x - x_recon).mean().item()
-    # log_det_error_step = torch.abs(log_det + log_det_inv).mean().item()
-
-    # print(f"FlowStep Reconstruction Error: {recon_error_step:.2e}")
-    # print(f"FlowStep Log-Determinant Sum: {log_det_error_step:.2e}")
-    # if recon_error_step < 1e-5 and log_det_error_step < 1e-5:
-    #     print("[STATUS] PASSED")
-    # else:
-    #     print("[STATUS] FAILED")
-
-
-    # --- Test DGLOWNetwork Invertibility ---
-    print("\n--- Testing DGLOWNetwork Invertibility ---")
-    model = Glow(
-        in_channels=3,
-        input_shape=(32, 32),
-        num_levels=3,
-        steps_per_level=[4, 4, 4],
-        hidden_channels=64,
-        scale_clamp=1.0,
-    )
-    x = torch.randn(8, 3, 32, 32).clamp(min=0.0, max=1.0)
-    z, log_det = model(x)
-    print(f"Output Latent Shapes: {[zi[1].shape for zi in z]}")
-    print(model.output_shapes)
-    
-    # # The inverse method should also reconstruct the image
-    # x_recon, log_det_inv = model.inverse(z)
-
-    # recon_error_net = torch.abs(x - x_recon).mean().item()
-    # print(f"DGLOWNetwork Reconstruction Error: {recon_error_net:.2e}")
-    # if recon_error_net < 1e-5:
-    #     print("[STATUS] PASSED")
-    # else:
-    #     print("[STATUS] FAILED")
+    pass
