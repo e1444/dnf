Fixed log det calculation

src/models/modules.py
@@ -170,7 +170,9 @@ class AffineCouplingLayer(nn.Module):
         self.register_buffer("scale_clamp", torch.tensor(scale_clamp))
 
     def forward(self, x):
-        x, total_log_det = self.actnorm(x)
+        total_log_det = torch.zeros(x.size(0), device=x.device)
+        x, log_det = self.actnorm(x)
+        total_log_det = total_log_det + log_det
         x, log_det = self.permute(x)
         total_log_det = total_log_det + log_det
         
@@ -185,7 +187,8 @@ class AffineCouplingLayer(nn.Module):
         y_b = x_b * s + t
         y = torch.cat([x_a, y_b], dim=1)
         log_det = torch.sum(torch.log(s), dim=[1, 2, 3])
-        return y, log_det
+        total_log_det = total_log_det + log_det
+        return y, total_log_det
 
     def inverse(self, y):
         y_a, y_b = self.split(y, method="split")

src/models/prior.py
@@ -5,7 +5,7 @@ from src.models.modules import Conv2dZeros, Split
 
 
 class LearnedPrior(nn.Module):
-    def __init__(self, shape, scale=3.0, cov_method="diag"):
+    def __init__(self, shape, scale=1.0, cov_method="diag"):
         """
         shape: (C, H, W) - Shape of the latent variable z
         """
@@ -39,7 +39,7 @@ class LearnedPrior(nn.Module):
     
     
 class ConditionalPrior(nn.Module):
-    def __init__(self, in_channels: int, out_channels: int, scale=3.0, cov_method: str = "diag"):
+    def __init__(self, in_channels: int, out_channels: int, scale=1.0, cov_method: str = "diag"):
         super(ConditionalPrior, self).__init__()
         self.in_channels = in_channels
         self.out_channels = out_channels