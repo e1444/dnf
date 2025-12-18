4b6d71969a4f05caa96a9e5f9467153dfdae2544
Eric Liang <eliangca@outlook.com>
2025-12-18 15:52:48 -0800

    Fixed bug

--- src/models/glow.py
+++ src/models/glow.py
@@ -78,7 +78,7 @@ class DGLOWNetwork(nn.Module):
                 torch.ones(C, device=std_per_level[0].device) * torch.mean(std_per_level[level_idx])
                 for _ in range(self.steps_per_level[level_idx])
             ]
-            std_per_step[-1][:C // 2] = std_per_level[level_idx]
+            std_per_step[-1][C // 2:] = std_per_level[level_idx]
             
             level_flows = nn.ModuleList([
                 FlowStep(
